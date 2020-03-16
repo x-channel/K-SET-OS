457906a885af0b5f373c7eae43cd9dfc37ecdeef
x-channel <cairecr@hotmail.com>
2020-03-16 12:08:08 -0300

    Escalonador

--- C/OS/Kernel/Escalonador.py
+++ C/OS/Kernel/Escalonador.py
@@ -19,16 +19,25 @@ class Escalonador(threading.Thread):
     def run(self):
         ##O escalonador eh um loop infinito
         while 1:
-            self.escalonar()
+            self.ordenar()
             for i in self.fila:
                 ##O escalonador escolhe o Processo
                 ##O escalonador reorganiza a fila
                 ##Inicia o timer
-                print(time.monotonic())
-                ##loop while, com as condicoes de tempo < self.quantum and falta de excecao
-                    ##Dentro do loop eh verificado se o processo.pause()
-                        ##Caso processo.pause() == True, o escalonador chama processo.passo()
-                    ##Finalmente o escalonador atualiza o tempo.
+                ##print(time.monotonic())
+                s0 = time.monotonic()
+                se = s0
+                try:
+                    ##loop while, com as condicoes de tempo < self.quantum and falta de excecao and not end
+                    while (s0 + self.quantum) > se:
+                        ##Dentro do loop eh verificado se o processo.pause()
+                        if i.pause():
+                            ##Caso processo.pause() == True, o escalonador chama processo.passo()
+                            i.passo()
+                        ##Finalmente o escalonador atualiza o tempo.
+                        se = time.monotonic()
+                except:
+                    print("Houve um erro")
                 ##Finalmente o escalonador atualiza o tempo decorrido dentro do Processo
         pass
 
@@ -39,5 +48,5 @@ class Escalonador(threading.Thread):
         ##Todas as referencias sao apagadas das listas tabela e fila
         pass
 
-    def escalonar(self, algoritmo = None):
+    def ordenar(self, algoritmo = None):
         pass
